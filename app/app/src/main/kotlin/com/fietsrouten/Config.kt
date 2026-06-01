6e27c63e25e3638cae89a1f01156969241d9c933
Dane Drake <dane.drake102@gmail.com>
2026-06-01 21:32:11 +0200

    Adding missing style config

--- app/app/src/main/kotlin/com/fietsrouten/Config.kt
+++ app/app/src/main/kotlin/com/fietsrouten/Config.kt
@@ -6,9 +6,9 @@ object Config {
     // (e.g. "192.168.1.100") and add that IP to res/xml/network_security_config.xml.
     const val SERVER_HOST = "10.0.2.2"
 
-    const val GRAPHHOPPER_BASE_URL = "http://$SERVER_HOST:8989/"
-    const val NOMINATIM_BASE_URL = "http://$SERVER_HOST:7070/"
+    const val GRAPHHOPPER_BASE_URL = "https://fietsnav-routing.learndelingo.nl"
+    const val NOMINATIM_BASE_URL = "https://fietsnav-geocoding.learndelingo.nl"
 
     // Vector tile URL served by Tileserver-GL from netherlands.mbtiles
-    const val MAP_STYLE_URL = "http://$SERVER_HOST:8080/styles/basic-preview/style.json"
+    const val MAP_STYLE_URL = "https://fietsnav-tile.learndelingo.nl/styles/basic-preview/style.json"
 }
